2b73ac1b2aa613697a38ac13fe6859180756efbb
kkini90 <kkini1990@gmail.com>
2017-02-20 19:30:07 -0800

    Create Binary Search Tree Iterator.java

---
+++ Medium/Binary Search Tree Iterator.java
@@ -0,0 +1,54 @@
+/*
+Implement an iterator over a binary search tree (BST). Your iterator will be initialized with the root node of a BST.
+
+Calling next() will return the next smallest number in the BST.
+
+Note: next() and hasNext() should run in average O(1) time and uses O(h) memory, where h is the height of the tree.
+*/
+------------------------------------------------------------------------------------------------
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+/*
+I use Stack to store directed left children from root. When next() be called, I just pop one element and process its right child as new root. The code is pretty straightforward.
+
+So this can satisfy O(h) memory, hasNext() in O(1) time, But next() is O(h) time.
+
+I can't find a solution that can satisfy both next() in O(1) time, space in O(h).
+*/
+public class BSTIterator {
+    private Stack<TreeNode> stack = new Stack<TreeNode>();
+    
+    public BSTIterator(TreeNode root) {
+        pushAll(root);
+    }
+
+    /** @return whether we have a next smallest number */
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+
+    /** @return the next smallest number */
+    public int next() {
+        TreeNode tempNode = stack.pop();
+        pushAll(tempNode.right);
+        return tempNode.val;
+    }
+    
+    private void pushAll(TreeNode node){
+        for(;node!=null;stack.push(node),node=node.left);
+        }
+    }
+}
+
+/**
+ * Your BSTIterator will be called like this:
+ * BSTIterator i = new BSTIterator(root);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
