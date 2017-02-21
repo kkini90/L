bfe5c39c57970b9656e1fca02033c1dd652958b1
kkini90 <kkini1990@gmail.com>
2017-02-20 17:08:48 -0800

    Create Ransom Note.java

---
+++ Easy/Ransom Note.java
@@ -0,0 +1,35 @@
+/*
+Given an arbitrary ransom note string and another string containing letters from all the magazines, write a function that will return true if the ransom note can be constructed from the magazines ; otherwise, it will return false.
+
+Each letter in the magazine string can only be used once in your ransom note.
+
+Note:
+You may assume that both strings contain only lowercase letters.
+
+canConstruct("a", "b") -> false
+canConstruct("aa", "ab") -> false
+canConstruct("aa", "aab") -> true
+*/
+------------------------------------------------------------------------------------------------
+public class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        if(ransomNote == null || ransomNote.length() == 0){
+            return true;
+        }
+        if(magazine == null || magazine.length() == 0){
+            return false;
+        }
+        
+        char[] count = new char[256];
+        for(char c : magazine.toCharArray()){
+            count[c]++;
+        }
+        for(char t  : ransomNote.toCharArray()){
+            if(count[t] == 0){
+                return false;
+            }
+            count[t]--;
+        }
+        return true;
+    }
+}
