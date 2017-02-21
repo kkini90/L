a55b22a2b77cae146fad6b21cf24ab4d6e7499c4
kkini90 <kkini1990@gmail.com>
2017-02-20 19:49:26 -0800

    Create Bitwise AND of Numbers Range.java

---
+++ Medium/Bitwise AND of Numbers Range.java
@@ -0,0 +1,14 @@
+/*
+Given a range [m, n] where 0 <= m <= n <= 2147483647, return the bitwise AND of all numbers in this range, inclusive.
+
+For example, given the range [5, 7], you should return 4.
+*/
+------------------------------------------------------------------------------------------------
+public class Solution {
+    public int rangeBitwiseAnd(int m, int n) {
+        while(m<n){
+            n= n &(n-1);
+        }
+        return n;
+    }
+}
