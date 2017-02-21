680f6506a2e945c5d29d88037aa68b6a4e7f19e3
kkini90 <kkini1990@gmail.com>
2017-02-20 20:01:52 -0800

    Create Minimum Size Subarray Sum.java

---
+++ Medium/Minimum Size Subarray Sum.java
@@ -0,0 +1,29 @@
+/*
+Given an array of n positive integers and a positive integer s, 
+find the minimal length of a contiguous subarray of which the sum ≥ s. If there isn't one, return 0 instead.
+
+For example, given the array [2,3,1,2,4,3] and s = 7,
+the subarray [4,3] has the minimal length under the problem constraint.
+*/
+------------------------------------------------------------------------------------
+public class Solution {
+    public int minSubArrayLen(int s, int[] nums) {
+        if(nums == null || nums.length==0){
+            return 0;
+        }
+        int i=0,j=0,sum =0, min =Integer.MAX_VALUE;
+        
+        //find total sum
+        while(j<nums.length){
+            sum+=nums[j++];
+        
+        //reduce reach sum from array
+        while(sum>=s){
+            min = Math.min(min,j-i);
+            sum-=nums[i++];
+        }
+     }
+        
+        return min == Integer.MAX_VALUE ? 0 : min;
+    }
+}
