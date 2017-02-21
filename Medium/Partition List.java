2688c24a5c0ee11921ce3487baf88fcc4a42d36e
kkini90 <kkini1990@gmail.com>
2017-02-20 19:56:28 -0800

    Create Partition List.java

---
+++ Medium/Partition List.java
@@ -0,0 +1,46 @@
+/*
+Given a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+
+You should preserve the original relative order of the nodes in each of the two partitions.
+
+For example,
+Given 1->4->3->2->5->2 and x = 3,
+return 1->2->2->4->3->5.
+*/
+------------------------------------------------------------------------------------------------
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode dummy1 = new ListNode(0);
+        ListNode dummy2 = new ListNode(0);
+        ListNode curr1 = dummy1;
+        ListNode curr2 = dummy2;
+        
+        while(head!=null){
+            if(head.val<x){
+                curr1.next = head;
+                curr1 = head;
+            }else{
+                curr2.next = head;
+                curr2 = head;
+            }
+            head = head.next;
+        }
+        curr2.next = null;
+        curr1.next = dummy2.next;
+        return dummy1.next;
+    }
+}
+
+/*
+the basic idea is to maintain two queues, the first one stores all nodes with val less than x , 
+and the second queue stores all the rest nodes. Then concat these two queues. 
+Remember to set the tail of second queue a null next, or u will get TLE.
+*/
