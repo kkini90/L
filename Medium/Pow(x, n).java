28a49ceb0a156bc47610e2cfbbc348441f231165
kkini90 <kkini1990@gmail.com>
2017-02-20 20:06:37 -0800

    Create Pow(x, n).java

---
+++ Medium/Pow(x, n).java
@@ -0,0 +1,24 @@
+/*
+Implement pow(x, n).
+*/
+------------------------------------------------------------------------------------
+public class Solution {
+    public double myPow(double x, int n) {
+       double temp = x;
+       if(n == 0){
+           return 1;
+       }
+       temp = myPow(x,n/2);
+       if(n%2 == 0){
+           return temp*temp;
+       }else{
+           if(n>0){
+               return temp*temp*x;
+           }else{
+               return (temp*temp)/x;
+           }
+       }
+       
+    }
+}
+// time complexity : log(n)
